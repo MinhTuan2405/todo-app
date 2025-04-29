40f9f5063e3d0404d7910f71bc8bfe34c5681128
mnhtndev <tuannguyen.02042005@gmail.com>
2025-04-29 23:06:11 +0700

    chore: config in local app

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss ()],
+  server: {
+    host: '0.0.0.0', // Cho phép truy cập từ các máy khác trong mạng
+    port: 5173,       // (tùy chọn) cổng mặc định của Vite
+  },
 })
